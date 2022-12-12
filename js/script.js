b4d5eb8d16ac8b192d2030f5d2d5eccb316cbf83
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-12-12 19:15:57 +0000

    clear count

--- js/script.js
+++ js/script.js
@@ -18,10 +18,20 @@ if (navigator.serviceWorker) {
 
 function count() {
   console.log("click")
-  let count = localStorage.getItem("number")
+  let count = localStorage.getItem("number");
   count++
-  localStorage.setItem("number", count)
+  localStorage.setItem("number", count);
   console.log(count)
 
   document.getElementById("answer").innerHTML = "The count is : " + count
 }
+
+function reset() {
+    console.log("clear count")
+    let count = 0
+    localStorage.setItem("number", count);
+    console.log(count)
+  
+    document.getElementById("answer").innerHTML = "The count is : " + count
+}
+
